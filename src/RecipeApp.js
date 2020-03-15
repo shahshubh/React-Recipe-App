f120a69cb920851e85d19afa581d3174be6b51af
shubh <shahshubh1010@gmail.com>
2020-03-15 11:35:50 +0530

    added navbar component and styles

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 //import logo from './logo.svg';
 import Recipe from './Recipe'
+import Navbar from './Navbar'
+
 import './RecipeApp.css';
 
 function RecipeApp() {
   return (
     <div className="App">
+      <Navbar />
       <Recipe 
         title="Pasta" 
         ingredients={['flour','water']}
