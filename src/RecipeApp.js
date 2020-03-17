cbbe5110b449d7fd7d0520baf5ad878acb7e90f7
shubh <shahshubh1010@gmail.com>
2020-03-17 11:31:42 +0530

    added state

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Navbar from './Navbar'
 import RecipeList from './RecipeList'
 
 import './RecipeApp.css';
 
-function RecipeApp() {
-  return (
-    <div className="App">
-      <Navbar />
-      <RecipeList />
-      <RecipeList />
-    </div>  
-  );
+class RecipeApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: 'pizza',
+          instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          ingredients: ['bread','cheese','chilli','toppings'],
+          image: 'pizza.jpeg'
+        },
+        {
+          id: 2,
+          title: 'noodles',
+          instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          ingredients: ['noodles','soya sauce','chilli sauce'],
+          image: 'noodles.jpeg'
+        },
+        {
+          id: 3,
+          title: 'sandwich',
+          instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          ingredients: ['breads','tomato','cheese','capsicum'],
+          image: 'sandwich.jpeg'
+        }
+      ],
+      nextRecipeId: 3,
+    }
+
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <RecipeList recipes={this.state.recipes} />
+      </div>  
+    );
+  }
 }
 
 export default RecipeApp;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -6,7 +6,7 @@ import './RecipeList.css';
 class RecipeList extends Component{
     render(){
         const recipes = this.props.recipes.map((r,index) => (
-            <Recipe key={index} {...r} />
+            <Recipe key={r.id} {...r} />
         ));
         return(
             <div className="recipe-list">
@@ -16,28 +16,6 @@ class RecipeList extends Component{
     }
 }
 
-RecipeList.defaultProps ={
-    recipes: [
-        {
-            title: 'pizza',
-            instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-            ingredients: ['bread','cheese','chilli','toppings'],
-            image: 'pizza.jpeg'
-        },
-        {
-            title: 'noodles',
-            instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-            ingredients: ['noodles','soya sauce','chilli sauce'],
-            image: 'noodles.jpeg'
-        },
-        {
-            title: 'sandwich',
-            instructions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-            ingredients: ['breads','tomato','cheese','capsicum'],
-            image: 'sandwich.jpeg'
-        }
-    ]
-}
 
 RecipeList.propTypes = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired
