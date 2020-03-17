d93e06ac05cd5fe611e2cb71e8fb428daa784927
shubh <shahshubh1010@gmail.com>
2020-03-17 14:35:33 +0530

    added show/hide form

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,25 @@
 import React ,{Component} from 'react';
-import './Navbar.css'
+import PropTypes from 'prop-types';
+import './Navbar.css';
 
 class Navbar extends Component{
+    static defaultProps = {
+        onNewRecipe(){}
+    }
+
+    static propTypes = {
+        onNewRecipe: PropTypes.func
+    }
+
     render(){
         return(
             <header>
                 <h2><a href="#">Recipe App</a></h2>
                 <nav>
-                    <li><a href="#">New Recipe</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact Us</a></li>
+                    <li><a onClick={this.props.onNewRecipe}>New Recipe</a></li>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact Us</a></li>
                 </nav>
             </header>
         );
--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -34,6 +34,7 @@ class RecipeApp extends Component {
         }
       ],
       nextRecipeId: 3,
+      showForm: false
     }
     this.handleSave = this.handleSave.bind(this);
   }
@@ -43,16 +44,23 @@ class RecipeApp extends Component {
       const newRecipe = {...recipe, id: this.state.nextRecipeId};
       return {
         nextRecipeId: prevState.nextRecipeId + 1,
-        recipes: [...this.state.recipes, newRecipe]
+        recipes: [...this.state.recipes, newRecipe],
+        showForm: false
       }
     });
   }
 
   render(){
+    const {showForm} = this.state;
     return (
       <div className="App">
-        <Navbar />
-        <RecipeInput onSave={this.handleSave} />
+        <Navbar onNewRecipe={() => this.setState({showForm: true})} />
+        { showForm ? 
+            <RecipeInput  
+              onSave={this.handleSave} 
+              onClose={() => this.setState({showForm: false})}
+            /> : 
+            null }
         <RecipeList recipes={this.state.recipes} />
       </div>  
     );
